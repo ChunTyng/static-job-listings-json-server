e84b97f5f199283a43354353ac04318fc9df07c0
chuntyng.ng <chuntyng.ng@atoz-software.tech>
2025-04-29 16:06:57 +0800

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve the data.json file
+app.get('/data', (req, res) => {
+  fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading data file');
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
