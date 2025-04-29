e25fd8c869cb9d9c4ecd64eaf2fec48a704e2389
chuntyng.ng <chuntyng.ng@atoz-software.tech>
2025-04-29 16:27:48 +0800

    updated

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -6,14 +5,17 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve the data.json file
+app.get('/', (req, res) => {
+  res.send('Welcome to the static job listings API! Access data at /data');
+});
+
 app.get('/data', (req, res) => {
   fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
     if (err) {
       return res.status(500).send('Error reading data file');
     }
-    res.json(JSON.parse(data));
-    res.send('Welcome to the static job listings API! Access data at /data');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(JSON.parse(data), null, 2));
   });
 });
 
